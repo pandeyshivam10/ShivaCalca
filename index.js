5f4cfeee80d5d4c5ea4b30c75cb2b7677eba41c2
pandeyshivam10 <85758924+pandeyshivam10@users.noreply.github.com>
2022-07-22 13:36:38 +0530

    change

--- index.js
+++ index.js
@@ -3,17 +3,18 @@ buttons = document.querySelectorAll('button');
 
 let inputonscreen = '';
 
-for(item of buttons) {
+for (item of buttons) {
+
+
 
     item.addEventListener('click', (e) => {
 
         buttonText = e.target.innerText;
-       
+
         if (buttonText == 'X') {
             buttonText = '*';
-            if(inputonscreen[0]=='0' && inputonscreen.length==1)
-            {
-                inputonscreen='';
+            if (inputonscreen[0] == '0' && inputonscreen.length == 1) {
+                inputonscreen = '';
             }
             inputonscreen += buttonText;
             screen.value = inputonscreen;
@@ -22,33 +23,76 @@ for(item of buttons) {
             inputonscreen = "0";
             screen.value = inputonscreen;
         }
-       else if(buttonText=='←')
-        {
-            if(!(inputonscreen[0]=='0' && inputonscreen.length==1))
-            {
-                
-                inputonscreen=inputonscreen.substring(0,inputonscreen.length-1);
+        else if (buttonText == 'sin') {
+
+            screen.value = Math.sin(inputonscreen);
+        }
+        else if (buttonText == 'log') {
+            screen.value = Math.log(inputonscreen);
+
+        }
+        else if (buttonText == 'cos') {
+            screen.value = Math.cos(inputonscreen);
+
+        }
+        else if (buttonText == 'tan') {
+            screen.value = Math.tan(inputonscreen);
+
+        }
+        else if (buttonText == '√') {
+            screen.value = Math.sqrt(inputonscreen);
+
+        }
+        else if (buttonText == '∛') {
+            screen.value = Math.cbrt(inputonscreen);
+
+        }
+        else if (buttonText == 'π') {
+
+            screen.value = screen.value + '3.14';
+            inputonscreen += '3.14';
+
+
+        }
+        else if (buttonText == 'X²') {
+            screen.value = Math.pow(inputonscreen, 2);
+
+        }
+        else if (buttonText == 'x!') {
+
+            n = inputonscreen;
+            f = 1;
+
+            for (i = 2; i <= n; i++) {
+
+                f = f * i;
+            }
+            screen.value = f;
+
+        }
+        else if (buttonText == 'DEL') {
+            if (!(inputonscreen[0] == '0' && inputonscreen.length == 1)) {
+
+                inputonscreen = inputonscreen.substring(0, inputonscreen.length - 1);
                 screen.value = inputonscreen;
             }
         }
-        
         else if (buttonText == '=') {
-            try{
+            try {
 
                 screen.value = eval(inputonscreen);
-                
-            }catch
+
+            } catch
             {
-                screen.value='ERROR!';
+                screen.value = 'ERROR!';
             }
         }
         else {
 
-            if(inputonscreen[0]=='0' && inputonscreen.length==1)
-            {
-                inputonscreen='';
+            if (inputonscreen[0] == '0' && inputonscreen.length == 1) {
+                inputonscreen = '';
             }
-           
+
             inputonscreen += buttonText;
             screen.value = inputonscreen;
         }
